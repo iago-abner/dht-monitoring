ec39e15d49064fe33ab40056e06839be7e526fde
iago_abner <iagoabner.dev@gmail.com>
2022-11-11 02:23:34 -0300

    Create api

---
+++ pages/api/temp.ts
@@ -0,0 +1,23 @@
+import { randomUUID } from 'crypto'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const arr: Array<any>= []
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const method = req.method
+
+  switch(method) {
+    case "GET":
+      res.status(200).json(arr)
+      break
+    case "POST":
+      const data = req.body
+      arr.push({value: data.temperature, date: new Date().toLocaleDateString() , id: randomUUID()})
+      res.status(201).json(arr)
+      break
+    default:
+      res.setHeader("Allow", ["GET", "POST"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+      break;
+  }
+}
