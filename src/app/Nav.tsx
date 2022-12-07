68432ac9c63f5569f19506dcc465857955d6582b
Iago Abner <iagoabner.dev@gmail.com>
2022-12-07 09:48:22 -0300

    fix: text color navbar

--- src/app/Nav.tsx
+++ src/app/Nav.tsx
@@ -20,7 +20,7 @@ export function Nav() {
             <li>
               <Link
                 href="/"
-                className="block py-2 pl-3 pr-4 focus:text-white hover:opacity-90 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block focus:text-white hover:opacity-90 py-2 pl-3 pr-4 text-gray-400 dark:text-gray-400  bg-transparent"
               >
                 Início
               </Link>
@@ -28,7 +28,7 @@ export function Nav() {
             <li>
               <Link
                 href="/temperature"
-                className="block focus:text-white hover:opacity-90 py-2 pl-3 pr-4  text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block focus:text-white hover:opacity-90 py-2 pl-3 pr-4 text-gray-400 dark:text-gray-400  bg-transparent"
               >
                 Temperatura
               </Link>
