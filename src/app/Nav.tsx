daf49afb0e32d07f67428a4b247a439cdb0ca2da
Iago Abner <iagoabner.dev@gmail.com>
2022-12-07 09:43:00 -0300

    fix: text color navbar

--- src/app/Nav.tsx
+++ src/app/Nav.tsx
@@ -36,7 +36,7 @@ export function Nav() {
             <li>
               <Link
                 href="/humidity"
-                className="block focus:text-white hover:opacity-90 py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block focus:text-white hover:opacity-90 py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Umidade
               </Link>
